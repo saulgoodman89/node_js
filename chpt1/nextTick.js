bc08a15d9df45688164125d239f0f70e15b64439
EungGyo <eungkyo89@gmail.com>
2026-01-09 12:54:00 +0900

    chpt1 132

---
+++ chpt1/nextTick.js
@@ -0,0 +1,13 @@
+setImmediate(() => {
+    console.log('immediate');
+});
+
+process.nextTick(() => {
+    console.log('nextTick');
+});
+
+setTimeout(() => {
+    console.log('timeout');
+},0);
+
+Promise.resolve().thien(() => console.log('promise'));
